a402cbf55f736f769750e02915a1becfe4f79df6
Ge Jian Feng <gejianfeng2@126.com>
2018-01-17 00:13:07 +0800

    + part finish index.php

--- en/index.php
+++ en/index.php
@@ -9,18 +9,104 @@ Template Name: index.php
 
 <div class="index-header-container">
 	<?php get_top_banner(); ?>
+
+	<div class='index-part1-container box-container'>
+		<div class="wrap">
+			<div class="index-part1-logo"></div>
+			<div class="index-part1-name page-sub-title text-white">Goldpebble</div>
+			<div class="index-part1-main text-white">Investing Technology Company</div>
+			<div class="index-part1-content text-white">Building Investing Edge <br/>with Quantamental Data Technology</div>
+		</div>
+	</div>
 </div>
 
 <div class="index-business-container">
+	<div class='index-part2-container box-container'>
+		<div class="wrap">
+			<div class="index-part2-sub-container index-part2-research">
+				<div class="page-sub-title index-part2-title text-white">Quantamental Research   <font size="5">➔</font></div>
+				<div class="index-part2-line">
+					<hr/>
+				</div>
+				<div class="index-part2-icon index-part2-icon-QR"></div>
+				<div class="index-part2-icon index-part2-icon-AD"></div>
+				<div class="sec-title index-part2-title-QR text-white">Quantamental Research</div>
+				<div class="sec-content index-part2-content-QR text-white">Fundamental Investors</div>
+				<div class="sec-title index-part2-title-AD text-white">Alternative Data</div>
+				<div class="sec-content index-part2-content-AD text-white">Strategic Investors</div>
+			</div>
+			<div class="index-part2-sub-container index-part2-investing">
+				<div class="page-sub-title index-part2-title text-white">Alternative Investing   <font size="5">➔</font></div>
+				<div class="index-part2-line">
+					<hr/>
+				</div>
+				<div class="index-part2-icon index-part2-icon-BC"></div>
+				<div class="index-part2-icon index-part2-icon-LE"></div>
+				<div class="sec-title index-part2-title-BC text-white">Cryptocurrencies</div>
+				<div class="sec-title index-part2-title-LE text-white">Live Events</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <div class="index-blockchain-container">
+	<div class='index-part3-container box-container'>
+		<div class="wrap">
+			<div class="page-sub-title index-part3-title-BC text-darkblue">Blockchain Technology</div>
+			<div class="darkblue index-part3-title-line"></div>
+			<div class="index-part3-BC-frame index-part3-BC-frame1">
+				<div class="index-part3-BC-frame-icon index-part3-BC-frame-icon1"></div>
+				<div class="sec-title index-part3-BC-frame-title text-darkblue">Decentralization</div>
+				<div class="sec-content index-part3-BC-frame-content text-darkblue">Secure with Distributed <br/>System</div>
+			</div>
+			<div class="index-part3-BC-frame index-part3-BC-frame2">
+				<div class="index-part3-BC-frame-icon index-part3-BC-frame-icon2"></div>
+				<div class="sec-title index-part3-BC-frame-title text-darkblue">Openess</div>
+				<div class="sec-content index-part3-BC-frame-content text-darkblue">Trust with Transparency</div>
+			</div>
+			<div class="index-part3-BC-frame index-part3-BC-frame3">
+				<div class="index-part3-BC-frame-icon index-part3-BC-frame-icon3"></div>
+				<div class="sec-title index-part3-BC-frame-title text-darkblue">Ecosystem</div>
+				<div class="sec-content index-part3-BC-frame-content text-darkblue">Grow with Collective <br/>Self Interest</div>
+			</div>
+			<div class="index-part3-info index-part3-info1">
+				<div class="index-part3-icon index-part3-icon1"></div>
+				<div class="index-part3-info-title text-white">Data</div>
+				<div class="index-part3-info-content content text-white">With the development of data storage and processing capabilities, large scale data and information can now be digitized and stored. Search engine technology and crowdsourcing make data easily accessible.</div>
+			</div>
+			<div class="index-part3-info index-part3-info2">
+				<div class="index-part3-icon index-part3-icon2"></div>
+				<div class="index-part3-info-title text-white">Edge</div>
+				<div class="index-part3-info-content content text-white">Goldpebble excels in bottoms-up quantamental analysis through primary data acquisition and cutting edge technology.</div>
+			</div>
+			<div class="index-part3-info index-part3-info3">
+				<div class="index-part3-icon index-part3-icon3"></div>
+				<div class="index-part3-info-title text-white">Technology</div>
+				<div class="index-part3-info-content content text-white">Using propreitary quantamental algorithms, Goldpebble acquires the most relevant data through traditional company filings, media news and economic data, as well as, company websites, social networks and big data.</div>
+			</div>
+			<div class="index-part3-read-more">
+				<div class="index-part3-read-more-text text-white">Read more</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <div class="index-achievements-container">
+	<div class='index-part4-container box-container'>
+		<div class="wrap">
+			<div class="page-sub-title index-part4-title text-white">Main Achievements</div>
+			<div class="white index-part4-title-line"></div>
+		</div>
+	</div>
 </div>
 
 <div class="index-news-container">
+	<div class='index-part5-container box-container'>
+		<div class="wrap">
+			<div class="page-sub-title index-part5-title text-darkblue">News Report</div>
+			<div class="darkblue index-part5-title-line"></div>
+		</div>
+	</div>
 </div>
 
 <!-- index.php end -->
