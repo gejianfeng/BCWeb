f9d50cfad7d1e443ed913300be18f241a7b6ad0b
Ge Jian Feng <gejianfeng2@126.com>
2018-01-17 21:29:40 +0800

    + add banner link

--- en/functions.php
+++ en/functions.php
@@ -12,38 +12,38 @@ function get_top_banner()
 			$ret.= "<span class='banner-company-name'><b>Goldpebble Research</b></span>";
 
 	if (is_home()) {
-			$ret.= "<div class='banner-nav-home'>HOME</div>";
+			$ret.= "<div class='banner-nav-home-pressed'>HOME</div>";
 	} 
 	else {
-			$ret.= "<div class='banner-nav-home text-transparent'>HOME</div>";
+			$ret.= "<div class='banner-nav-home' style='cursor:pointer' onclick='SwitchPage(0)'><div class='text-transparent'>HOME</div></div>";
 	}
 
 	if (is_page('quantamentalresearch')) {
-			$ret.= "<div class='banner-nav-research'>Quantamental Research</div>";
+			$ret.= "<div class='banner-nav-research-pressed'>Quantamental Research</div>";
 	} 
 	else {
-			$ret.= "<div class='banner-nav-research text-transparent'>Quantamental Research</div>";
+			$ret.= "<div class='banner-nav-research' style='cursor:pointer' onclick='SwitchPage(1)'><div class='text-transparent'>Quantamental Research</div></div>";
 	}
 
 	if (is_page('alternativeinvesting')) {
-			$ret.= "<div class='banner-nav-investing'>Alternative Investing</div>";
+			$ret.= "<div class='banner-nav-investing-pressed'>Alternative Investing</div>";
 	} 
 	else {
-			$ret.= "<div class='banner-nav-investing text-transparent'>Alternative Investing</div>";
+			$ret.= "<div class='banner-nav-investing' style='cursor:pointer' onclick='SwitchPage(2)'><div class='text-transparent'>Alternative Investing</div></div>";
 	}
 
 	if (is_page('blockchaintechnology')) {
-			$ret.= "<div class='banner-nav-blockchain'>Blockchain Technology</div>";
+			$ret.= "<div class='banner-nav-blockchain-pressed'>Blockchain Technology</div>";
 	} 
 	else {
-			$ret.= "<div class='banner-nav-blockchain text-transparent'>Blockchain Technology</div>";
+			$ret.= "<div class='banner-nav-blockchain' style='cursor:pointer' onclick='SwitchPage(3)'><div class='text-transparent'>Blockchain Technology</div></div>";
 	}
 
 	if (is_page('career')) {
-			$ret.= "<div class='banner-nav-career'>Careers</div>";
+			$ret.= "<div class='banner-nav-career-pressed'>Careers</div>";
 	} 
 	else {
-			$ret.= "<div class='banner-nav-career text-transparent'>Careers</div>";
+			$ret.= "<div class='banner-nav-career' style='cursor:pointer' onclick='SwitchPage(4)'><div class='text-transparent'>Careers</div></div>";
 	}
 
 		$ret.= "</div>";
--- en/header.php
+++ en/header.php
@@ -13,6 +13,33 @@ Template Name: header.php
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 		<link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 		<script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js" type="text/javascript"></script>
+
+		<script type="text/javascript">
+			function SwitchPage(index)
+			{
+				$target_link = '';
+				switch(index)
+				{
+					case 0:
+						$target_link = '/index.php';
+						break;
+					case 1:
+						$target_link = '/index.php/quantamentalresearch/';
+						break;
+					case 2:
+						$target_link = '/index.php/alternativeinvesting/';
+						break;
+					case 3:
+						$target_link = '/index.php/blockchaintechnology/';
+						break;
+					case 4:
+						$target_link = '/index.php/career/';
+						break;
+				}
+
+				window.location.href='<?php echo home_url(); ?>' + $target_link;
+			}
+		</script>
 	</head>
 
 	<body>
